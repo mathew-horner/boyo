3c7dd7a25f150978b3d623153acf3057b713c6f0
Mathew Horner <mathewhorner456@gmail.com>
2021-04-29 21:59:27 -0500

    gb: Better way to discard cycles.

--- src/gb/debugger.rs
+++ src/gb/debugger.rs
@@ -11,9 +11,7 @@ impl Debugger {
 
     pub fn step(&mut self) -> Result<(), TickError> {
         // Discard cycle count because we don't care about it when in debug mode.
-        match self.gameboy.tick() {
-            Ok(_) => Ok(()),
-            Err(error) => Err(error)
-        }
+        self.gameboy.tick()?;
+        Ok(())
     }
 }
