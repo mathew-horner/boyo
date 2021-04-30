0dcfcceca6ce283ad3dbd62cb643d5036c2abc00
Mathew Horner <mathewhorner456@gmail.com>
2021-04-29 23:05:30 -0500

    gb: Working on adding breakpoints to the debugger.

--- src/gb/debugger.rs
+++ src/gb/debugger.rs
@@ -1,16 +1,49 @@
 use crate::{ Gameboy, TickError };
+use std::fmt;
 
 pub struct Debugger {
     pub gameboy: Gameboy,
+    breakpoints: Vec<Breakpoint>,
 }
 
 impl Debugger {
     pub fn new(gameboy: Gameboy) -> Self {
-        Self { gameboy }
+        Self { gameboy, breakpoints: Vec::new() }
     }
 
     pub fn invoke_command(&mut self, command: &str) {
-        match command {
+        let tokens: Vec<&str> = command.split(' ').collect();
+        match tokens[0] {
+            "break-add" => {
+                match Self::parse_break_address(&tokens) {
+                    Ok(address) => {
+                        self.breakpoints.push(Breakpoint::new(address));
+                        println!("Breakpoint added at address: {:#X}", address);
+                    },
+                    Err(error) => println!("{}", error),
+                }
+            },
+            "break-list" => {
+                for breakpoint in self.breakpoints.iter() {
+                    println!("{:#X}", breakpoint.address);
+                }
+            },
+            "break-remove" => {
+                match Self::parse_break_address(&tokens) {
+                    Ok(address) => {
+                        // TODO-CQ: Could probably use a more FP approach here.
+                        for (idx, breakpoint) in self.breakpoints.iter().enumerate() {
+                            if breakpoint.address == address {
+                                self.breakpoints.remove(idx);
+                                println!("Breakpoint removed at address: {:#X}", address);
+                                return;
+                            }
+                        }
+                        println!("No breakpoint exists at address: {:#X}", address);
+                    },
+                    Err(error) => println!("{}", error),
+                }
+            },
             "exit" => std::process::exit(0),
             "help" => {
                 println!("---------------");
@@ -61,5 +94,39 @@ impl Debugger {
         // Discard cycle count because we don't care about it when in debug mode.
         self.gameboy.tick()?;
         Ok(())
+    }
+
+    fn parse_break_address(tokens: &Vec<&str>) -> Result<u16, ParseBreakAddressError> {
+        if tokens.len() != 2 || tokens[1].len() < 3 || &tokens[1][..2] != "0x" {
+            return Err(ParseBreakAddressError::BadUsage { command: String::from(tokens[0]) });
+        }
+        match u16::from_str_radix(&tokens[1][2..], 16) {
+            Ok(address) => Ok(address),
+            Err(error) => Err(ParseBreakAddressError::BadParse { error }),
+        }
+    }
+}
+
+struct Breakpoint {
+    address: u16,
+}
+
+impl Breakpoint {
+    pub fn new(address: u16) -> Self {
+        Self { address }
+    }
+}
+
+enum ParseBreakAddressError {
+    BadUsage { command: String },
+    BadParse { error: std::num::ParseIntError }
+}
+
+impl fmt::Display for ParseBreakAddressError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match self {
+            Self::BadUsage { command } => format!("Usage: {} <address (ex: 0x1000)>", command),
+            Self::BadParse { error } => format!("Error parsing address: {}", error),
+        })
     }
 }
